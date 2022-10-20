c8328866fa683b7c095f85dda8bf1e0105c7e278
roniana11 <roniana11@gmail.com>
2022-10-20 11:23:22 +0300

    adding queries

--- database.sql
+++ database.sql
@@ -1,22 +1,21 @@
 USE poketracker;
+-- create database poketracker;
 
--- -- create database poketracker;
+-- Create table pokemon(
+--     id int Not null primary key,
+--     name varchar(20),
+--     height int,
+--     weight int
+-- );
 
--- -- Create table pokemon(
--- --     id int Not null primary key,
--- --     name varchar(20),
--- --     height int,
--- --     weight int
--- -- );
+-- create table trainer(
+--     name varchar(20) not null primary key,
+--     town varchar(20)
+-- );
 
--- -- create table trainer(
--- --     name varchar(20) not null primary key,
--- --     town varchar(20)
--- -- );
-
--- -- create table types(
--- --     name varchar(20) not null primary key
--- -- );
+-- create table types(
+--     name varchar(20) not null primary key
+-- );
 
 -- create table pokemon_types(
 --     pokemon_id int not null,
@@ -26,13 +25,31 @@ USE poketracker;
 --     primary key (pokemon_id, type_name) 
 -- );
 
--- -- create table pokemon_trainer(
--- --     trainer_name varchar(20) not null,
--- --     pokemon_id int not null,
--- --     foreign key(pokemon_id) references pokemon(id),
--- --     foreign key(trainer_name) references trainer(name),
--- --     primary key(trainer_name, pokemon_id) 
+-- create table pokemon_trainer(
+--     trainer_name varchar(20) not null,
+--     pokemon_id int not null,
+--     foreign key(pokemon_id) references pokemon(id),
+--     foreign key(trainer_name) references trainer(name),
+--     primary key(trainer_name, pokemon_id) 
 
--- -- );
+-- );
 
-select * from types;
+-- INSERT into pokemon(id, name, height, weight) values(1, "charmer", 5,7);
+-- INSERT into pokemon(id, name, height, weight) values(2, "charm", 5,4);
+-- INSERT into pokemon(id, name, height, weight) values(3, "charme", 5,9);
+
+-- INSERT into trainer(name, town) values('rano','bdkbs');
+-- INSERT into trainer(name, town) values('ran','bds');
+-- INSERT into trainer(name, town) values('ro','dkbs');
+
+-- INSERT INTO pokemon_trainer(trainer_name,pokemon_id) VALUES("rano",1);
+-- INSERT INTO pokemon_trainer(trainer_name,pokemon_id) VALUES("rano",2);
+-- INSERT INTO pokemon_trainer(trainer_name,pokemon_id) VALUES("ran",1);
+-- INSERT INTO pokemon_trainer(trainer_name,pokemon_id) VALUES("rano",3);
+
+-- select * from pokemon_trainer;
+
+-- SELECT trainer_name,pokemon_id,name,id 
+SELECT trainer_name 
+FROM pokemon AS p ,pokemon_trainer AS pt
+WHERE p.id = pt.pokemon_id AND p.name = "charmer"
--- insert_data.py
+++ insert_data.py
@@ -63,3 +63,19 @@ def insert_type(name):
 # insert_pokemon(1, "balbazor", 100, 60)
 # insert_trainer("ash", "tel-aviv")
 insert_type("fire")
+
+def setup_db(data):
+    trainers = {}
+    types = {}
+    for pokemon in data:
+        insert_pokemon(pokemon["id"],pokemon["name"],pokemon["height"],pokemon["weight"])
+        insert_pokemon_type()
+        for trainer in pokemon["ownedBy"]:
+            unique_key = trainer["name"]+trainer["town"]
+            trainers[unique_key] = trainer
+            types.add(pokemon["type"])
+
+    for trainer in trainers:
+        insert_trainer(trainer["name"],trainer["town"])
+    for type in types:
+        insert_type(type)
